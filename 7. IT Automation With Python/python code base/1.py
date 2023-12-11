352b58e4d147a4907bd0bbdff068731ed37b32fc
ShahNawaz2323 <shahnawazwarriach2323@gmail.com>
2023-12-11 06:34:01 +0500

    python first practice

---
+++ 7. IT Automation With Python/python code base/1.py
@@ -0,0 +1,6 @@
+friends = ['Taylor', 'Alex', 'Pat', 'Eli']
+for friend in friends:
+    print("Hi " + friend)
+friends1 = ['shah', 'Hammad', 'Ahmad']
+for friend in friends1:
+    print("Have a good day! dear, "+ friend)
